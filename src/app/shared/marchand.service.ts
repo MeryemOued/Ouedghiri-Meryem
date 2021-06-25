ace0435f4d99bbfe9278a2b1181ca1c8944acb9b
MeryemOued <ouedghiri4meryem@gmail.com>
2021-06-25 12:05:10 +0100

    UP

--- src/app/shared/marchand.service.ts
+++ src/app/shared/marchand.service.ts
@@ -5,7 +5,7 @@ import { saveAs } from 'file-saver';
 import jsPDF from 'jspdf';
 import { jsDocComment } from '@angular/compiler';
 import { Observable, throwError } from 'rxjs';
-import {catchError, map} from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
 import { ModalRef } from 'ng-zorro-antd-mobile';
 import { Platform } from './model/platform';
 import { Activity } from './model/activity';
@@ -14,7 +14,6 @@ import { Activity } from './model/activity';
   providedIn: 'root',
 })
 export class MarchandService {
-
   constructor(private httpClient: HttpClient) {}
   // marchand : Marchand;
   readonly baseURL = 'https://localhost:44341/api/Marchand';
@@ -26,60 +25,70 @@ export class MarchandService {
   ma: any;
   ///////////PLATFORM////////////
   getPlatform() {
-    return this.httpClient.get(`https://localhost:44341/api/Platform`).subscribe(res=>{
-      console.log(res);
-    });
+    return this.httpClient
+      .get(`https://localhost:44341/api/Platform`)
+      .subscribe((res) => {
+        console.log(res);
+      });
   }
   ////////
   refreshTable() {
     this.httpClient
       .get(this.baseURL)
       .toPromise()
-      .then((res) => {this.list = res as unknown as Marchand[]});
-  } 
+      .then((res) => {
+        this.list = res as unknown as Marchand[];
+      });
+  }
   refreshPlatform() {
     this.httpClient
       .get(`https://localhost:44341/api/Platform`)
       .toPromise()
-      .then((res) => {this.listPlatform = res as unknown as Platform[]
-      console.log(res)
-      })
-       ;
-  } 
+      .then((res) => {
+        this.listPlatform = res as unknown as Platform[];
+        console.log(res);
+      });
+  }
 
   refreshActivity() {
     this.httpClient
       .get(`https://localhost:44341/api/Activite`)
       .toPromise()
-      .then((res) => {this.listActivity = res as unknown as Activity[]
-      console.log(res)
-      })
-       ;
-  } 
+      .then((res) => {
+        this.listActivity = res as unknown as Activity[];
+        console.log(res);
+      });
+  }
   public upload(formData: FormData) {
-    return this.httpClient.post(`https://localhost:44341/api/Attachement/upload`, formData,{
-      reportProgress: true,
-      observe: 'events', 
-    });
-    
+    return this.httpClient.post(
+      `https://localhost:44341/api/Attachement/upload`,
+      formData,
+      {
+        reportProgress: true,
+        observe: 'events',
+      }
+    );
   }
 
   createAttachement(data: any) {
-    console.log(data)
-    console.log("attach")
-    return this.httpClient.post(`https://localhost:44341/api/Attachement`, data);
+    console.log(data);
+    console.log('attach');
+    return this.httpClient.post(
+      `https://localhost:44341/api/Attachement`,
+      data
+    );
   }
 
-   file ="7al"
+  file = '7al';
 
   createService(data: any) {
-    console.log(data)
-  return  this.httpClient.post(this.baseURL,data)
+    console.log(data);
+    return this.httpClient.post(this.baseURL, data);
   }
 
   putService(data: any) {
-    console.log("PUT")
-    console.log(data)
+    console.log('PUT');
+    console.log(data);
     return this.httpClient.put(`${this.baseURL}`, data);
   }
   getService() {
@@ -123,7 +132,7 @@ export class MarchandService {
   }
 
   //////////////////////// CASH PLUS /////////////////////////////
-  public postToken(data:any) {
-   return this.httpClient.post(`https://localhost:44341/api/Operation`,data)
+  public postToken(data: any) {
+    return this.httpClient.post(`https://localhost:44341/api/Operation`, data);
   }
 }
